a434026460776c33048f6a53dee71d5ecf8061e6
vladislavlarchev <143349392+vladislavlarchev@users.noreply.github.com>
2023-09-04 12:03:02 +0000

    Updated

--- assets/js/clock.js
+++
@@ -1,7 +0,0 @@
-(() => {
-    function updateClock() {
-        const clockContainer = document.querySelector('.clock');
-        clockContainer.innerText = new Date().toLocaleTimeString('uk');
-    }
-    setInterval(updateClock, 1000);
-})();
